0c4908cd09d818467f81ce5c3b2f50d07c729083
sammus9474 <52513636+sammus9474@users.noreply.github.com>
2020-07-01 12:17:39 -0600

    Initial commit

---
+++ circle.java
@@ -0,0 +1,16 @@
+
+
+public class circle extends DrawingPanel
+{
+    void main (String[] args)
+    {
+        
+        DrawingPanel Cricle = new DrawingPanel(20 , 20);
+        
+    }
+    public circle (int h , int w){
+    
+        super( h , w);
+    }
+    
+}
